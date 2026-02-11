867a738a1a158b5c06032760f4e7face4513f2a1
kinghardesh <agarwalhardesh06@gmail.com>
2026-02-11 23:44:24 +0530

    starting with login page

--- week3/flask/smartexpense/app.py
+++ week3/flask/smartexpense/app.py
@@ -1,7 +1,11 @@
 from flask import Flask,render_template,redirect
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
-from model import spending,user
+from flask_login import usermixin
+from flask_wtf import wtforms
+from wtforms import StringField,PasswordField,SubmitField
+from wrforms.validators import inputRequired,Length,validatorsError
+
 app=Flask(__name__)
 app.config['SQLAlchemy_Database_uri']='sqlite:///spending.db'
 db=SQLAlchemy(app)
@@ -14,16 +18,34 @@ class spending(db.Model):
     def __repr__(self):
         return f"<spending {self.id}>"
 
-class user(db.Model):
+class user(db.Model,):
     id=db.Column(db.Integer,primary_key=True)
     name=db.Column(db.String(30), nullable=False)
     password_hash=db.Column(db.String(30), nullable=False)
+
+class registerForm(wtforms.Form):
+    name=StringField('name',validators=[inputRequired(),Length(min=4,max=30)])
+    password=PasswordField('password',validators=[inputRequired(),Length(min=4,max=30)])
+    submit=SubmitField('Register')
+    def validate_name(self,name):
+        existing_user=user.query.filter_by(name=name.data).first()
+        if existing_user:
+            raise validatorsError('Username already exists. Please choose a different one.')
+class loginForm(wtforms.Form):
+    name=StringField('name',validators=[inputRequired(),Length(min=4,max=30)])
+    password=PasswordField('password',validators=[inputRequired(),Length(min=4,max=30)])
+    submit=SubmitField('Login')
     
-@app.route('/', methods=('get','post'))
+
+@app.route('/')
+def home():
+    return render_template('basic_page.html')
+    
+@app.route('/form', methods=('get','post'))
 def index():
     if request.method=='post':
         content=request.form['content']
-        amount=request.form['amount']
+        amount=int(request.form['amount'])
         category=request.form['category']
         new_task=spending(
             content=content,
@@ -50,3 +72,8 @@ def signup():
 @app.route('/logout')
 def logout():
     return 'logout page'
+
+if __name__=="__main__":
+    with app.app_context():
+        db.create_all()
+    app.run(debug=True)
